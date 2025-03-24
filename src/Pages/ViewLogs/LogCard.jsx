1b1992c7770a71798ff647aa0823d45a3fd35be7
Muhammad Ahmad <its_ahmad_nyc@yahoo.com>
2025-03-24 14:28:31 +0500

    Map Added

--- src/Pages/ViewLogs/LogCard.jsx
+++ src/Pages/ViewLogs/LogCard.jsx
@@ -1,29 +1,91 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 import { useTheme } from '../../Contexts/ThemeContext';
+import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-fullscreen';
 
-function LogCard(logsData) {
-  const { theme } = useTheme()
+
+const mapContainerStyle = {
+  width: '100%',
+  height: '200px',
+};
+
+const LogCard = ({ data }) => {
+  const { theme } = useTheme();
+
+  function formatTimestampCustom(timestamp) {
+    const date = new Date(timestamp);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} `;
+  }
+
+  const startCoords = data?.lat_long_start?.split(',').map(Number);
+  const endCoords = data?.lat_long_end?.split(',').map(Number);
+
+  const path = [
+    [startCoords[0], startCoords[1]],
+    [endCoords[0], endCoords[1]],
+  ];
+
   return (
     <motion.div
-      className={`rounded-xl w-[100%] p-5  shadow 
-                    ${theme === "dark" ? "bg-[#323335] border-[#323335]" : "bg-white border-[#ECECEC]"}`}
+      className={`rounded-xl w-[100%] p-5 shadow-lg ${theme === "dark" ? "bg-[#323335] border-[#323335]" : "bg-white border-[#ECECEC] border"
+        }`}
       initial={{ scale: 0.5 }}
       animate={{ scale: 1 }}
       exit={{ scale: 0.8 }}
       transition={{ duration: 0.3 }}
     >
-      <div className='w-full flex items-center justify-center'>
+      <div className="w-full flex items-center justify-center">
         <p className={`text-[1.3rem] font-medium ${theme === 'dark' ? 'text-white' : 'text-black'}`}>View Logs</p>
       </div>
-      <div>
-        <div>
-          <p className={`text-[#777e90] font-medium text-[1.2rem]`}>Start Time:</p>
-          <p className={`${theme==="dark"?"text-white":"text-black"}`}></p>
+
+      <div className="flex lg:items-center justify-between gap-3 lg:flex-row flex-col">
+        <div className="flex flex-col gap-3 mt-5">
+          <div className="flex items-center gap-1">
+            <p className="text-[#777e90] font-medium">Start Time:</p>
+            <p className={theme === "dark" ? "text-white" : "text-black"}>{formatTimestampCustom(Number(data?.start_time))}</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <p className="text-[#777e90] font-medium">End Time:</p>
+            <p className={theme === "dark" ? "text-white" : "text-black"}>{formatTimestampCustom(Number(data?.end_time))}</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 lg:mt-5">
+          <div className="flex items-center gap-1">
+            <p className="text-[#777e90] font-medium">Start Mileage:</p>
+            <p className={theme === "dark" ? "text-white" : "text-black"}>{data?.start_mileage}</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <p className="text-[#777e90] font-medium">End Mileage:</p>
+            <p className={theme === "dark" ? "text-white" : "text-black"}>{data?.end_mileage}</p>
+          </div>
         </div>
       </div>
-    </motion.div>
-  )
-}
 
-export default LogCard
+      <div className="flex items-center gap-1 mt-2">
+        <p className="text-[#777e90] font-medium">Total distance driven:</p>
+        <p className={theme === "dark" ? "text-white" : "text-black"}>{data?.total_distance}</p>
+      </div>
+
+      {startCoords && endCoords && (
+        <div className="mt-2">
+          <p className="text-[#777e90] font-medium mb-2">Map:</p>
+          <MapContainer
+            center={startCoords}
+            zoom={15}
+            style={mapContainerStyle}
+            whenCreated={(map) => map.addControl(new L.Control.Fullscreen())}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={startCoords} />
+            <Marker position={endCoords} />
+            <Polyline positions={path} color="red" />
+          </MapContainer>
+        </div>
+      )}
+    </motion.div>
+  );
+};
+
+export default LogCard;
--- src/Pages/ViewLogs/ViewLogs.jsx
+++ src/Pages/ViewLogs/ViewLogs.jsx
@@ -1,41 +1,48 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import LogCard from './LogCard'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { getLogs } from '../../API/portalServices';
 import { useParams } from 'react-router-dom';
+import { BeatLoader } from 'react-spinners';
+import NoDataFound from '../../GlobalComponents/NoDataFound/NoDataFound';
 
 function ViewLogs() {
-   const [loading, setLoading] = useState(false);
-    const [logsData, setLogsData] = useState({})
-    const {id} = useParams();
-  
-    const fetchLogsData = useCallback(async () => {
-      setLoading(true);
-      try {
-        const response = await getLogs(id);
-        setLogsData(response?.data?.data || {});
-      } catch (error) {
-        console.error("Error fetching dashboard data:", error);
-      } finally {
-        setLoading(false);
-      }
-    }, []);
-    
-    useEffect(() => {
-      fetchLogsData();
-    }, []);
+  const [loading, setLoading] = useState(false);
+  const [logsData, setLogsData] = useState({})
+  const { id } = useParams();
+
+  const fetchLogsData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await getLogs(id);
+      setLogsData(response?.data?.data || {});
+    } catch (error) {
+      console.error("Error fetching dashboard data:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchLogsData();
+  }, []);
+
   return (
     <motion.div
-    initial="hidden"
-    animate="visible"
-    variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
->
-    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-        <LogCard logsData={logsData}/>
+      initial="hidden"
+      animate="visible"
+      variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
+    >
+      {loading ? <div className="h-[80vh] flex items-center justify-center">
+        <BeatLoader color="#2d9bff" />
+      </div> : (logsData.length > 0 ? (
+        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-4'>
+          {logsData.map((data, index) => (
+            <LogCard data={data} key={index} />))}
 
-    </div>
+        </div>) : <div className="h-[80vh] flex items-center justify-center"><NoDataFound /></div>)}
 
-</motion.div>
+    </motion.div>
   )
 }
 
