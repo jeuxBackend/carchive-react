56f0bd60f74d8f3623d16082dc582e91b7e56212
Muhammadazeem37 <140012372+Muhammadazeem37@users.noreply.github.com>
2025-04-24 12:40:58 +0500

    apply theme

--- src/Pages/Requests/GarageCard.jsx
+++ src/Pages/Requests/GarageCard.jsx
@@ -20,7 +20,6 @@ function GarageCard({ data, fetchRequestsData }) {
     } catch (error) {
       console.error(error.response)
       toast.error(error.response.data.message)
-
     } finally {
       setLoading(false)
     }
@@ -43,36 +42,36 @@ function GarageCard({ data, fetchRequestsData }) {
         <div className="bg-gradient-to-b from-transparent to-black/80 w-full h-full absolute rounded-xl top-0" />
         <p className='text-[1.4rem] font-medium bottom-3 absolute left-3 text-white'>{data?.garage?.name}</p>
       </motion.div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>Email:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.email}</p> 
+      
+      
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>Email:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.email}</p> 
       </div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>Phone:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.phNumber}</p> 
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>Phone:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.phNumber}</p> 
       </div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>City:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.city}</p> 
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>City:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.city}</p> 
       </div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>ZipCode:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.zipCode}</p> 
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>ZipCode:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.zipCode}</p> 
       </div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>Street:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.street}</p> 
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>Street:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.street}</p> 
       </div>
-      <div className='pt-2 flex justify-between'>
-          <p className='fontbold text-lg text-[#323335]'>House No:</p>
-         <p className='text-[#7B7B7B]'>{data?.garage?.houseNum}</p> 
+      <div className={`pt-2 flex justify-between ${theme === "dark" ? "text-white" : ""}`}>
+          <p className='fontbold text-lg'>House No:</p>
+          <p className={theme === "dark" ? "text-gray-300" : "text-[#7B7B7B]"}>{data?.garage?.houseNum}</p> 
       </div>
+
       <div className="mt-2 flex gap-4 sm:flex-row flex-col">
-
         <GradientButton name='Reject' handleClick={() => requestStatus(data?.id, "4")} loading={loading} />
-
         <RequestButton name="Accept" handleClick={() => requestStatus(data?.id, "2")} loading={loading} />
-
       </div>
     </motion.div>
   )
