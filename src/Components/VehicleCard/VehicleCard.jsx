534cfebe1508824ec604cf88359986db16e21656
Muhammad Ahmad <its_ahmad_nyc@yahoo.com>
2025-05-27 15:58:17 +0500

    refactor: Simplify background and text color logic in VehicleCard component

--- src/Components/VehicleCard/VehicleCard.jsx
+++ src/Components/VehicleCard/VehicleCard.jsx
@@ -8,23 +8,7 @@ function VehicleCard({ data }) {
     const { theme } = useTheme();
     const { vehicle, setVehicle } = useGlobalContext();
 
-    const getCardBackgroundColor = () => {
-        if (data?.expired === '1') {
-            return 'bg-red-100 border-red-600 border-2'; 
-        } else if (data?.month_expiry === '1') {
-            return 'bg-yellow-100 border-yellow-600 border-2'; 
-        } else {
-            return theme === "dark" ? "bg-[#323335]" : "bg-white border border-[#ececec]";
-        }
-    };
-
-    const getTextColor = () => {
-        if (data?.expired === '1' || data?.month_expiry === '1') {
-            return 'text-black'; 
-        } else {
-            return theme === 'dark' ? "text-white" : "text-black";
-        }
-    };
+   
 
     const renderExpiryBadge = () => {
         if (data?.expired === '1') {
@@ -51,12 +35,12 @@ function VehicleCard({ data }) {
                 transition={{ duration: 0.5 }}
                 onClick={() => setVehicle(data)}
                 whileHover={{ scale: 1.05, boxShadow: "0px 5px 15px rgba(0,0,0,0.2)" }}
-                className={`${getCardBackgroundColor()} rounded-xl shadow-md p-3 h-[18rem] md:h-[14.5rem] flex flex-col gap-1 relative transition-all duration-300`}
+                className={`${theme === "dark" ? "bg-[#323335]" : "bg-white border border-[#ececec]"} rounded-xl shadow-md p-3 h-[18rem] md:h-[14.5rem] flex flex-col gap-1 relative transition-all duration-300`}
             >
                 {renderExpiryBadge()}
                 <p className='text-[#2d9bff] font-medium'>{data?.make}</p>
-                <p className={`font-medium ${getTextColor()}`}>{data?.vinNumber}</p>
-                <p className={`font-medium ${getTextColor()}`}>{data?.numberPlate}</p>
+                <p className={`font-medium ${theme === 'dark' ? "text-white" : "text-black"}`}>{data?.vinNumber}</p>
+                <p className={`font-medium ${theme === 'dark' ? "text-white" : "text-black"} `}>{data?.numberPlate}</p>
                 <img src={data?.image ? data?.image[0] : ""} alt="" className='w-[17rem] lg:w-[18rem] xl:w-[17rem] h-[10.5rem] absolute bottom-0 sm:bottom-2 right-0 border-none' />
             </motion.div>
         </Link>
